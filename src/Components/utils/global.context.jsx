33d68f43917eedf5b6d11322d4fca541f2c9afc9
drbh2020 <berastaindiego@gmail.com>
2024-12-07 00:48:57 -0500

    feat: global context and favs

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,7 +1,10 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import PropTypes from "prop-types";
 
+const lsFavs = JSON.parse(localStorage.getItem("favs")) || []
+
 export const initialState = {
+  favs: lsFavs,
   theme: "light",
   data: [],
 };
@@ -12,15 +15,25 @@ const reducer = (state, action) => {
       return { ...state, theme: state.theme === "light" ? "dark" : "light" };
     case "GET_DENTISTS":
       return { ...state, data: action.payload };
+      case "ADD_FAV":
+        return { ...state, favs: [...state.favs, action.payload] };
+    case "DELETE_FAV": {
+      const filterFavs = state.favs.filter((fav) => fav.id !== action.payload.id);
+      return { ...state, favs: filterFavs };
+    }
     default:
       return state;
-  }
+    }
 };
 
 export const ContextGlobal = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("favs", JSON.stringify(state.favs));
+  }, [state.favs]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -38,3 +51,5 @@ export const ContextProvider = ({ children }) => {
 ContextProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export const useContextGlobal = () => useContext(ContextGlobal)
